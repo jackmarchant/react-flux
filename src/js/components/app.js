10e23aa1de983d7762cc9794b6b0014b29e90324
Jack Marchant <jack@jackmarchant.com>
2015-12-13 13:41:57 +1100

    Added initial files

---
+++ src/js/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppActions from '../actions/app-actions';
+import Catalog from './app-catalog';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<Catalog/>
+			</div>
+		);
+	}
+}
